37f17a631ae4ea2507a82f181f70e5f34bfa2ad4
Alim Weber <alim.weber@outlook.com>
2025-03-04 17:36:57 +0100

    creat tmp file

--- polybar/update/update_status.sh
+++ polybar/update/update_status.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -n /tmp/polybar-script-output ]]; then
+	
+	touch /tmp/polybar-script-output
+
+fi
 
 if [[ "$(cat /tmp/polybar-script-output)" == "updating" ]]; then
 	echo "󰔪 "
